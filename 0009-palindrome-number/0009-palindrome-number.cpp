80cdbbdc04b835a00043036132c03064bbfddf62
Suraj Lanke <surajlanke45@gmail.com>
2025-09-28 21:08:04 +0530

    Time: 6 ms (17.72%), Space: 12.1 MB (5.01%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false; 
+    string s = to_string(x);
+    string rev = string(s.rbegin(), s.rend());
+    return s == rev;
+    }
+};
