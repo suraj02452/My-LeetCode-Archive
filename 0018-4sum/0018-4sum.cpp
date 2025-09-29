4014fe6467eae97405b7d3c7646269a6b7b37d99
Suraj Lanke <surajlanke45@gmail.com>
2025-09-29 11:20:34 +0530

    Time: 24 ms (35.99%), Space: 17.5 MB (55.5%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+         vector<vector<int>> ans;
+        int n = nums.size();
+        if (n < 4) return ans;
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n - 3; i++) {
+            // Skip duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            for (int j = i + 1; j < n - 2; j++) {
+                // Skip duplicates for the second number
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                
+                int left = j + 1;
+                int right = n - 1;
+                
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        
+                        // Skip duplicates for the third number
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        // Skip duplicates for the fourth number  
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        
+                        left++;
+                        right--;
+                    } 
+                    else if (sum < target) {
+                        left++;
+                    } 
+                    else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
